b6220e2f3fbaf1806b22e756473e775bba193292
Syaiful Sabril <syaiful.sabril@gmail.com>
2017-10-22 06:30:01 +0700

    add active leads

--- app/models/plugins/cama_contact_form/cama_campaign.rb
+++ app/models/plugins/cama_contact_form/cama_campaign.rb
@@ -5,6 +5,7 @@ class Plugins::CamaContactForm::CamaCampaign < ActiveRecord::Base
 	default_scope { order(created_at: :desc) }
 
 	has_many :campaign_steps, class_name: "Plugins::CamaContactForm::CamaCampaignStep", foreign_key: :campaign_id, dependent: :destroy
+	has_many :contacts_campaigns, class_name: "Plugins::CamaContactForm::CamaContactsCampaign", foreign_key: :campaign_id, dependent: :destroy
 
 	accepts_nested_attributes_for :campaign_steps, allow_destroy: true, reject_if: proc {|field| field[:time_frame].blank?}
 
@@ -16,5 +17,13 @@ class Plugins::CamaContactForm::CamaCampaign < ActiveRecord::Base
 			end
 		end
 		successful_number
+	end
+
+	def active_leads_number
+		contacts_campaigns_number = 0
+		contacts_campaigns.each do |contact_campaign|
+			contacts_campaigns_number += 1 if contact_campaign.contact_form.present?
+		end
+		contacts_campaigns_number
 	end
 end
