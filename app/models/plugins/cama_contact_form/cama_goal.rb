f9f388c1121eabc607623dfd10a0599ec9ddd2d5
Syaiful Sabril <syaiful.sabril@gmail.com>
2017-10-22 06:18:38 +0700

    add successfull campaign

--- app/models/plugins/cama_contact_form/cama_goal.rb
+++ app/models/plugins/cama_contact_form/cama_goal.rb
@@ -1,4 +1,13 @@
 class Plugins::CamaContactForm::CamaGoal < ActiveRecord::Base
 	self.table_name = 'goals'
 	belongs_to :site, class_name: "CamaleonCms::Site"
+	has_many :campaigns, class_name: "Plugins::CamaContactForm::CamaCampaign", foreign_key: :goal_id, dependent: :destroy
+
+	def successful_campaigns_number
+		successful_campaigns_number = 0
+		campaigns.each do |c| 
+			successful_campaigns_number += c.successful_number
+		end
+		successful_campaigns_number
+	end
 end
