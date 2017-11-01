6f851c76823d804c955ba7a4e7adac2cdbb888ca
Syaiful Sabril <syaiful.sabril@gmail.com>
2017-11-01 08:23:52 +0700

    update parser on template

--- app/models/plugins/cama_contact_form/cama_template.rb
+++ app/models/plugins/cama_contact_form/cama_template.rb
@@ -5,10 +5,17 @@ class Plugins::CamaContactForm::CamaTemplate < ActiveRecord::Base
 	attr_accessor :tmp_content
 
 	def get_content(contact, value)
+		current_site = site
+		current_theme = site.get_theme
 		email = value[:fields][contact.parent.fields.select{|f| f[:label].to_s.downcase == "email"}.first[:cid].to_sym]
 		name = value[:fields][contact.parent.fields.select{|f| f[:label].to_s.downcase == "name"}.first[:cid].to_sym]
 		content.gsub!("{{contact_name}}", name.titleize)
 		content.gsub!("{{contact_email}}", email)
+
+		content.gsub!("{{site_email}}", (current_theme.get_field_value('business_email') rescue ""))
+		content.gsub!("{{site_phone}}", (current_theme.get_field_value('business_phone') rescue ""))
+		content.gsub!("{{site_address}}", (current_theme.get_field_value('business_address') rescue ""))
+		content.gsub!("{{site_logo}}", ActionController::Base.helpers.image_tag(site.get_option('logo')))
 		content
 	end
 end
