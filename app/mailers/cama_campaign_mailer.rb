8c689c37b168f7826839c013fd331a3654f61be6
Syaiful Sabril <syaiful.sabril@gmail.com>
2017-11-08 21:14:03 +0700

    fix email from

--- app/mailers/cama_campaign_mailer.rb
+++ app/mailers/cama_campaign_mailer.rb
@@ -9,7 +9,7 @@ class CamaCampaignMailer < ApplicationMailer
   def send_content(contact_email, content, subject, campaign_step_id)
     @content = content
     admin = CamaleonCms::User.where(role: "admin").first
-    mail from: admin.email, to: contact_email, subject: subject, "X-Mailgun-Variables" => { campaign_step_id: campaign_step_id }.to_json
+    mail to: contact_email, subject: subject, "X-Mailgun-Variables" => { campaign_step_id: campaign_step_id }.to_json
   end
 
   def notify_admin(content, subject, campaign_step_id)
